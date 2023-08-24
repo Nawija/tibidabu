43f2754352791e06fd812f877751ada603d4c9ac
Valkon <96625039+Nawija@users.noreply.github.com>
2023-08-24 17:24:43 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,23 @@ const IndexPage = () => {
         <>
             <Layout>
                 <div className="mx-auto text-center overflow-hidden relative text-white max-w-[1700px]">
-                    {window.innerWidth >= 500 ? (
-                        <StaticImage
-                            loading="eager"
-                            imgClassName="xl:rounded-2xl"
-                            quality={100}
-                            placeholder="dominantColor"
-                            src="../images/hero.jpg"
-                        />
-                    ) : (
-                        <StaticImage
-                            loading="eager"
-                            quality={100}
-                            placeholder="dominantColor"
-                            src="../images/shero.jpg"
-                        />
-                    )}
+                    <StaticImage
+                        loading="eager"
+                        imgClassName="xl:rounded-2xl hidden sm:flex"
+                        className="hidden sm:flex"
+                        quality={100}
+                        placeholder="dominantColor"
+                        src="../images/hero.jpg"
+                    />
+
+                    <StaticImage
+                        loading="eager"
+                        quality={100}
+                        imgClassName="sm:hidden flex"
+                        className="sm:hidden flex"
+                        placeholder="dominantColor"
+                        src="../images/shero.jpg"
+                    />
                     <div className="absolute top-[45%] lg:top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center flex-col">
                         <p className=" bg-[#19C0B6] py-1.5 px-3 lg:py-2 lg:px-4 text-base tracking-wider lg:text-lg font-medium w-max rounded-lg">
                             WITAJ W TIBIDABU
