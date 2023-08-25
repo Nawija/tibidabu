7438ff7526d51a731938fce80f423eb05758b707
Valkon <96625039+Nawija@users.noreply.github.com>
2023-08-25 18:01:37 +0200

    edf

--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -4,29 +4,23 @@ import Seo from "../components/seo";
 import TopImg from "../components/TopImg";
 import { Link } from "gatsby";
 
-const NotFoundPage = () => {
+const ONasPage = () => {
     return (
         <Layout>
-            <TopImg head="Error" />
+            <TopImg head="O Nas" />
             <p className="py-12 font-bold text-center">Hello 404</p>
-
             <Link
                 to="/"
                 aria-label="Powrót Strona Główna"
                 title="Strona Główna"
                 className=" relative flex items-center justify-center py-2 font-medium bg-[#5271FF] text-white px-4 rounded-lg w-max mx-auto"
             >
-Strona Główna
+                Strona Główna
             </Link>
         </Layout>
     );
 };
 
-export default NotFoundPage;
+export default ONasPage;
 
-export const Head = () => (
-    <Seo
-        title="Nie znaleziono strony | Error 404"
-        siteUrl="https://seovileo.pl/404"
-    />
-);
+export const Head = () => <Seo title="O Nas" siteUrl="" />;
